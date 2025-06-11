b90f19cea4fd72763b9f85afcac23a8183322673
ousttrue <ousttrue@gmail.com>
2025-06-11 23:44:07 +0900

    main.cpp

--- samples/vko/main.cpp
+++ samples/vko/main.cpp
@@ -11,7 +11,12 @@ class Glfw {
 
 public:
   GLFWwindow *_window = nullptr;
-  Glfw() { glfwInit(); }
+  Glfw() {
+    glfwSetErrorCallback([](int error, const char *description) {
+      vko::Logger::Error("GLFW Error %d: %s\n", error, description);
+    });
+    glfwInit();
+  }
   ~Glfw() {
     if (_window) {
       glfwDestroyWindow(_window);
@@ -19,6 +24,10 @@ public:
     glfwTerminate();
   }
   void *createWindow(int width, int height, const char *title) {
+    if (!glfwVulkanSupported()) {
+      vko::Logger::Error("GLFW: Vulkan Not Supported\n");
+      return nullptr;
+    }
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     // glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
@@ -35,52 +44,6 @@ public:
     return true;
   }
 };
-
-//   glfwSetErrorCallback([](int error, const char *description) {
-//     vko::Logger::Error("GLFW Error %d: %s\n", error, description);
-//   });
-//   if (!glfwInit()) {
-//     return 1;
-//   }
-//   // Create window with Vulkan context
-//   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-//   GLFWwindow *window =
-//       glfwCreateWindow(1280, 720, WINDOW_TITLE, nullptr, nullptr);
-//   if (!glfwVulkanSupported()) {
-//     printf("GLFW: Vulkan Not Supported\n");
-//     return 1;
-//   }
-//
-//   {
-//     //
-//     // vulkan instance with debug layer
-//     //
-//     vko::Instance instance;
-//     // vulkan extension for glfw surface
-//     uint32_t extensions_count = 0;
-//     auto glfw_extensions = glfwGetRequiredInstanceExtensions(&extensions_count);
-//     for (uint32_t i = 0; i < extensions_count; i++) {
-//       instance.instanceExtensions.push_back(glfw_extensions[i]);
-//     }
-// #ifndef NDEBUG
-//     instance.validationLayers.push_back("VK_LAYER_KHRONOS_validation");
-//     instance.instanceExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-// #endif
-//     VKO_CHECK(instance.create());
-//
-//     //
-//     // vulkan device with glfw surface
-//     //
-//     VkSurfaceKHR _surface;
-//     VKO_CHECK(glfwCreateWindowSurface(instance, window, nullptr, &_surface));
-//     auto picked = instance.pickPhysicalDevice(_surface);
-//     vko::Surface surface(instance, _surface, picked.physicalDevice);
-//
-//     vko::Device device;
-//     device.validationLayers = instance.validationLayers;
-//     VKO_CHECK(device.create(picked.physicalDevice, picked.graphicsFamilyIndex,
-//                             picked.presentFamilyIndex));
-
 
 int main(int argc, char **argv) {
   std::vector<const char *> validationLayers;
