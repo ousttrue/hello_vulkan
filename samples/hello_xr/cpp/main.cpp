791405213fe25dd177bf0a6148ec4bca538a157c
ousttrue <ousttrue@gmail.com>
2025-06-13 17:29:46 +0900

    xr_loop

--- samples/hello_xr/cpp/main.cpp
+++ samples/hello_xr/cpp/main.cpp
@@ -1,3 +1,4 @@
+#include "openxr/openxr.h"
 #include "openxr_program/CubeScene.h"
 #include "openxr_program/VulkanDebugMessageThunk.h"
 #include "openxr_program/openxr_program.h"
@@ -23,6 +24,86 @@ void ShowHelp() {
   Log::Write(Log::Level::Info,
              "Environment blend modes:  Opaque, Additive, AlphaBlend");
   Log::Write(Log::Level::Info, "Spaces:                   View, Local, Stage");
+}
+
+void xr_loop(const std::function<std::tuple<bool, XrFrameState>()> &runLoop,
+             const std::shared_ptr<OpenXrSession> &session,
+             const Options &options, VkPhysicalDevice physicalDevice,
+             uint32_t queueFamilyIndex, VkDevice device) {
+
+  // Create resources for each view.
+  auto config = session->GetSwapchainConfiguration();
+  std::vector<std::shared_ptr<OpenXrSwapchain>> swapchains;
+  std::vector<std::shared_ptr<VulkanRenderer>> renderers;
+  for (uint32_t i = 0; i < config.Views.size(); i++) {
+    // XrSwapchain
+    auto swapchain = OpenXrSwapchain::Create(session->m_session, i,
+                                             config.Views[i], config.Format);
+    swapchains.push_back(swapchain);
+
+    // VkPipeline... etc
+    auto renderer = std::make_shared<VulkanRenderer>(
+        physicalDevice, device, queueFamilyIndex,
+        VkExtent2D{swapchain->m_swapchainCreateInfo.width,
+                   swapchain->m_swapchainCreateInfo.height},
+        static_cast<VkFormat>(swapchain->m_swapchainCreateInfo.format),
+        static_cast<VkSampleCountFlagBits>(
+            swapchain->m_swapchainCreateInfo.sampleCount));
+    renderers.push_back(renderer);
+  }
+
+  // mainloop
+  while (true) {
+    auto [run, frameState] = runLoop();
+    if (!run) {
+      break;
+    }
+
+    if (frameState.shouldRender != XR_TRUE) {
+      continue;
+    }
+
+    uint32_t viewCountOutput;
+    if (!session->LocateView(session->m_appSpace,
+                             frameState.predictedDisplayTime,
+                             options.Parsed.ViewConfigType, &viewCountOutput)) {
+      continue;
+    }
+
+    LayerComposition composition(options.Parsed.EnvironmentBlendMode,
+                                 session->m_appSpace);
+
+    // update scene
+    CubeScene scene;
+    scene.addSpaceCubes(session->m_appSpace, frameState.predictedDisplayTime,
+                        session->m_visualizedSpaces);
+    scene.addHandCubes(session->m_appSpace, frameState.predictedDisplayTime,
+                       session->m_input);
+
+    for (uint32_t i = 0; i < viewCountOutput; ++i) {
+      // XrCompositionLayerProjectionView(left / right)
+      auto swapchain = swapchains[i];
+      auto info = swapchain->AcquireSwapchain(session->m_views[i]);
+      composition.pushView(info.CompositionLayer);
+
+      {
+        // render vulkan
+        auto renderer = renderers[i];
+        VkCommandBuffer cmd = renderer->BeginCommand();
+        renderer->RenderView(cmd, info.Image, options.GetBackgroundClearColor(),
+                             scene.CalcCubeMatrices(info.calcViewProjection()));
+        renderer->EndCommand(cmd);
+      }
+
+      swapchain->EndSwapchain();
+    }
+
+    // std::vector<XrCompositionLayerBaseHeader *>
+    auto &layers = composition.commitLayers();
+    session->EndFrame(frameState.predictedDisplayTime, layers);
+  }
+
+  vkDeviceWaitIdle(device);
 }
 
 int main(int argc, char *argv[]) {
@@ -97,90 +178,35 @@ int main(int argc, char *argv[]) {
       program->InitializeSession(vulkan.Instance, vulkan.PhysicalDevice,
                                  vulkan.Device, vulkan.QueueFamilyIndex);
 
-  // Create resources for each view.
-  auto config = session->GetSwapchainConfiguration();
-  std::vector<std::shared_ptr<OpenXrSwapchain>> swapchains;
-  std::vector<std::shared_ptr<VulkanRenderer>> renderers;
-  for (uint32_t i = 0; i < config.Views.size(); i++) {
-    // XrSwapchain
-    auto swapchain = OpenXrSwapchain::Create(session->m_session, i,
-                                             config.Views[i], config.Format);
-    swapchains.push_back(swapchain);
-
-    // VkPipeline... etc
-    auto renderer = std::make_shared<VulkanRenderer>(
-        vulkan.PhysicalDevice, vulkan.Device, vulkan.QueueFamilyIndex,
-        VkExtent2D{swapchain->m_swapchainCreateInfo.width,
-                   swapchain->m_swapchainCreateInfo.height},
-        static_cast<VkFormat>(swapchain->m_swapchainCreateInfo.format),
-        static_cast<VkSampleCountFlagBits>(
-            swapchain->m_swapchainCreateInfo.sampleCount));
-    renderers.push_back(renderer);
-  }
-
-  // mainloop
-  while (!quitKeyPressed) {
-    bool exitRenderLoop = false;
-    bool requestRestart = false;
-    session->PollEvents(&exitRenderLoop, &requestRestart);
-    if (exitRenderLoop) {
-      break;
-    }
-
-    if (session->IsSessionRunning()) {
-      session->PollActions();
-
-      auto frameState = session->BeginFrame();
-      LayerComposition composition(options.Parsed.EnvironmentBlendMode,
-                                   session->m_appSpace);
-
-      if (frameState.shouldRender == XR_TRUE) {
-        uint32_t viewCountOutput;
-        if (session->LocateView(
-                session->m_appSpace, frameState.predictedDisplayTime,
-                options.Parsed.ViewConfigType, &viewCountOutput)) {
-          // XrCompositionLayerProjection
-
-          // update scene
-          CubeScene scene;
-          scene.addSpaceCubes(session->m_appSpace,
-                              frameState.predictedDisplayTime,
-                              session->m_visualizedSpaces);
-          scene.addHandCubes(session->m_appSpace,
-                             frameState.predictedDisplayTime, session->m_input);
-
-          for (uint32_t i = 0; i < viewCountOutput; ++i) {
-            // XrCompositionLayerProjectionView(left / right)
-            auto swapchain = swapchains[i];
-            auto info = swapchain->AcquireSwapchain(session->m_views[i]);
-            composition.pushView(info.CompositionLayer);
-
-            {
-              // render vulkan
-              auto renderer = renderers[i];
-              VkCommandBuffer cmd = renderer->BeginCommand();
-              renderer->RenderView(
-                  cmd, info.Image, options.GetBackgroundClearColor(),
-                  scene.CalcCubeMatrices(info.calcViewProjection()));
-              renderer->EndCommand(cmd);
-            }
-
-            swapchain->EndSwapchain();
+  xr_loop(
+      [session, &options]() -> std::tuple<bool, XrFrameState> {
+        while (true) {
+          if (quitKeyPressed) {
+            return {false, {}};
           }
+
+          bool exitRenderLoop = false;
+          bool requestRestart = false;
+          session->PollEvents(&exitRenderLoop, &requestRestart);
+          if (exitRenderLoop) {
+            return {false, {}};
+          }
+
+          if (!session->IsSessionRunning()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
+            continue;
+          }
+
+          break;
         }
-      }
 
-      // std::vector<XrCompositionLayerBaseHeader *>
-      auto &layers = composition.commitLayers();
-      session->EndFrame(frameState.predictedDisplayTime, layers);
+        session->PollActions();
 
-    } else {
-      // Throttle loop since xrWaitFrame won't be called.
-      std::this_thread::sleep_for(std::chrono::milliseconds(250));
-    }
-  }
-
-  vkDeviceWaitIdle(device);
+        auto frameState = session->BeginFrame();
+        return {true, frameState};
+      },
+      session, options, vulkan.PhysicalDevice, vulkan.QueueFamilyIndex,
+      vulkan.Device);
 
   return 0;
 }
