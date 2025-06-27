43ca12696204de6fc044ccb3f81483a09bcb8817
ousttrue <ousttrue@gmail.com>
2025-06-27 21:17:48 +0900

    vuloxr::vk::Instance

--- samples/vuloxr/05_cube/main_loop.cpp
+++ samples/vuloxr/05_cube/main_loop.cpp
@@ -24,13 +24,19 @@ Draw Cube
 */
 
 /* This is part of the draw cube progression */
+#include <Windows.h>
+
+#include <vulkan/vulkan.h>
+
+#include <vulkan/vulkan_win32.h>
 
 #include "cube_data.h"
 #include <assert.h>
-#include <iostream>
-#include <fstream>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <string.h>
+#include <vuloxr/vk.h>
 #include <vuloxr/vk/shaderc.h>
 
 #define GLM_FORCE_RADIANS
@@ -267,8 +273,7 @@ struct sample_info {
   VkViewport viewport;
   VkRect2D scissor;
 };
-void process_command_line_args(struct sample_info &info, int argc,
-                               char *argv[]);
+
 bool memory_type_from_properties(struct sample_info &info, uint32_t typeBits,
                                  VkFlags requirements_mask,
                                  uint32_t *typeIndex);
@@ -327,15 +332,9 @@ bool AndroidLoadFile(const char *filePath, std::string *data);
 
 VkResult init_global_extension_properties(layer_properties &layer_props);
 
-VkResult init_global_layer_properties(sample_info &info);
-
 VkResult init_device_extension_properties(struct sample_info &info,
                                           layer_properties &layer_props);
 
-void init_instance_extension_names(struct sample_info &info);
-VkResult init_instance(struct sample_info &info,
-                       char const *const app_short_name, const void *pNext);
-void init_device_extension_names(struct sample_info &info);
 VkResult init_device(struct sample_info &info);
 VkResult init_enumerate_device(struct sample_info &info,
                                uint32_t gpu_count = 1);
@@ -744,35 +743,6 @@ static bool optionMatch(const char *option, char *optionLine) {
     return true;
   else
     return false;
-}
-
-void process_command_line_args(struct sample_info &info, int argc,
-                               char *argv[]) {
-  int i, n;
-
-  for (i = 1, n = 1; i < argc; i++) {
-    if (optionMatch("--save-images", argv[i]))
-      info.save_images = true;
-    else if (optionMatch("--help", argv[i]) || optionMatch("-h", argv[i])) {
-      printf("\nOther options:\n");
-      printf("\t--save-images\n"
-             "\t\tSave tests images as ppm files in current working "
-             "directory.\n");
-      exit(0);
-    } else {
-      printf("\nUnrecognized option: %s\n", argv[i]);
-      printf("\nUse --help or -h for option list.\n");
-      exit(0);
-    }
-
-    /*
-     * Since the above "consume" inputs, update argv
-     * so that it contains the trimmed list of args for glutInit
-     */
-
-    argv[n] = argv[i];
-    n++;
-  }
 }
 
 void write_ppm(struct sample_info &info, const char *basename) {
@@ -1211,67 +1181,6 @@ VkResult init_global_extension_properties(layer_properties &layer_props) {
   return res;
 }
 
-/*
- * TODO: function description here
- */
-VkResult init_global_layer_properties(struct sample_info &info) {
-  uint32_t instance_layer_count;
-  VkLayerProperties *vk_props = NULL;
-  VkResult res;
-#ifdef __ANDROID__
-  // This place is the first place for samples to use Vulkan APIs.
-  // Here, we are going to open Vulkan.so on the device and retrieve function
-  // pointers using vulkan_wrapper helper.
-  if (!InitVulkan()) {
-    LOGE("Failied initializing Vulkan APIs!");
-    return VK_ERROR_INITIALIZATION_FAILED;
-  }
-  LOGI("Loaded Vulkan APIs.");
-#endif
-
-  /*
-   * It's possible, though very rare, that the number of
-   * instance layers could change. For example, installing something
-   * could include new layers that the loader would pick up
-   * between the initial query for the count and the
-   * request for VkLayerProperties. The loader indicates that
-   * by returning a VK_INCOMPLETE status and will update the
-   * the count parameter.
-   * The count parameter will be updated with the number of
-   * entries loaded into the data pointer - in case the number
-   * of layers went down or is smaller than the size given.
-   */
-  do {
-    res = vkEnumerateInstanceLayerProperties(&instance_layer_count, NULL);
-    if (res)
-      return res;
-
-    if (instance_layer_count == 0) {
-      return VK_SUCCESS;
-    }
-
-    vk_props = (VkLayerProperties *)realloc(
-        vk_props, instance_layer_count * sizeof(VkLayerProperties));
-
-    res = vkEnumerateInstanceLayerProperties(&instance_layer_count, vk_props);
-  } while (res == VK_INCOMPLETE);
-
-  /*
-   * Now gather the extension list for each instance layer.
-   */
-  for (uint32_t i = 0; i < instance_layer_count; i++) {
-    layer_properties layer_props;
-    layer_props.properties = vk_props[i];
-    res = init_global_extension_properties(layer_props);
-    if (res)
-      return res;
-    info.instance_layer_properties.push_back(layer_props);
-  }
-  free(vk_props);
-
-  return res;
-}
-
 VkResult init_device_extension_properties(struct sample_info &info,
                                           layer_properties &layer_props) {
   VkExtensionProperties *device_extensions;
@@ -1321,56 +1230,6 @@ VkBool32 demo_check_layers(const std::vector<layer_properties> &layer_props,
     }
   }
   return 1;
-}
-
-void init_instance_extension_names(struct sample_info &info) {
-  info.instance_extension_names.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
-#ifdef __ANDROID__
-  info.instance_extension_names.push_back(
-      VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
-#elif defined(_WIN32)
-  info.instance_extension_names.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
-#elif defined(VK_USE_PLATFORM_METAL_EXT)
-  info.instance_extension_names.push_back(VK_EXT_METAL_SURFACE_EXTENSION_NAME);
-#elif defined(VK_USE_PLATFORM_WAYLAND_KHR)
-  info.instance_extension_names.push_back(
-      VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME);
-#else
-  info.instance_extension_names.push_back(VK_KHR_XCB_SURFACE_EXTENSION_NAME);
-#endif
-}
-
-VkResult init_instance(struct sample_info &info,
-                       char const *const app_short_name, const void *pNext) {
-  VkApplicationInfo app_info = {};
-  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  app_info.pNext = NULL;
-  app_info.pApplicationName = app_short_name;
-  app_info.applicationVersion = 1;
-  app_info.pEngineName = app_short_name;
-  app_info.engineVersion = 1;
-  app_info.apiVersion = VK_API_VERSION_1_0;
-
-  VkInstanceCreateInfo inst_info = {};
-  inst_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  inst_info.pNext = pNext;
-  inst_info.flags = 0;
-  inst_info.pApplicationInfo = &app_info;
-  inst_info.enabledLayerCount = info.instance_layer_names.size();
-  inst_info.ppEnabledLayerNames = info.instance_layer_names.size()
-                                      ? info.instance_layer_names.data()
-                                      : NULL;
-  inst_info.enabledExtensionCount = info.instance_extension_names.size();
-  inst_info.ppEnabledExtensionNames = info.instance_extension_names.data();
-
-  VkResult res = vkCreateInstance(&inst_info, NULL, &info.inst);
-  assert(res == VK_SUCCESS);
-
-  return res;
-}
-
-void init_device_extension_names(struct sample_info &info) {
-  info.device_extension_names.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
 }
 
 VkResult init_device(struct sample_info &info) {
@@ -3464,54 +3323,22 @@ int sample_main(int argc, char *argv[]) {
   char sample_title[] = "Draw Cube";
   const bool depthPresent = true;
 
-  process_command_line_args(info, argc, argv);
-  init_global_layer_properties(info);
-  init_instance_extension_names(info);
-  init_device_extension_names(info);
+  // init_instance(info, sample_title, &debugUtilsMessengerCreateInfo);
+  vuloxr::vk::Instance instance;
+  instance.addLayer("VK_LAYER_KHRONOS_validation");
+  instance.addExtension(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+  instance.addExtension(VK_KHR_SURFACE_EXTENSION_NAME);
+  instance.addExtension(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
 
-  info.instance_layer_names.push_back("VK_LAYER_KHRONOS_validation");
-  info.instance_extension_names.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-  VkDebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfo = {
-      .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-      .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
-      .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-                     VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-                     VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
-      .pfnUserCallback =
-          [](VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
-             VkDebugUtilsMessageTypeFlagsEXT message_types,
-             const VkDebugUtilsMessengerCallbackDataEXT *callback_data,
-             void *user_data) {
-            if (message_severity &
-                VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
-              printf("%s %s\n", callback_data->pMessageIdName,
-                     callback_data->pMessage);
-            } else if (message_severity &
-                       VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
-              printf("%s %s\n", callback_data->pMessageIdName,
-                     callback_data->pMessage);
-            } else if (message_severity &
-                       VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT) {
-              printf("%s %s\n", callback_data->pMessageIdName,
-                     callback_data->pMessage);
-            } else /*if(message_severity &
-                      VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)*/
-            {
-              printf("%s %s\n", callback_data->pMessageIdName,
-                     callback_data->pMessage);
-            }
+  instance.create();
+  info.inst = instance;
 
-            // }
-            return VK_FALSE;
-          },
-  };
-  init_instance(info, sample_title, &debugUtilsMessengerCreateInfo);
   init_enumerate_device(info);
   init_window_size(info, 500, 500);
   init_connection(info);
   init_window(info);
   init_swapchain_extension(info);
+  info.device_extension_names.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
   init_device(info);
 
   init_command_pool(info);
@@ -3526,6 +3353,7 @@ int sample_main(int argc, char *argv[]) {
 
   const char VS[] = {
 #embed "15-draw_cube.vert"
+      , 0, 0, 0, 0,
   };
   auto vs = vuloxr::vk::glsl_vs_to_spv(VS);
   VkShaderModuleCreateInfo vert_info = {
@@ -3536,6 +3364,7 @@ int sample_main(int argc, char *argv[]) {
 
   const char FS[] = {
 #embed "15-draw_cube.frag"
+      , 0, 0, 0, 0,
   };
   auto fs = vuloxr::vk::glsl_fs_to_spv(FS);
   VkShaderModuleCreateInfo frag_info = {
@@ -3678,6 +3507,5 @@ int sample_main(int argc, char *argv[]) {
   destroy_command_pool(info);
   destroy_device(info);
   destroy_window(info);
-  destroy_instance(info);
   return 0;
 }
